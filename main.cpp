7e6f408b886a4efcfee35d5e2ca77f2e7d23f568
Tomoya Fujii <ftomoya2213@g.ecc.u-tokyo.ac.jp>
2024-09-05 17:05:20 +0900

    modeを追加

--- main.cpp
+++ main.cpp
@@ -1,13 +1,15 @@
 //-------------------------------------------------------------
 // To compile, perform: 
-// g++ -std=c++20 -Wall --pedantic-errors -o SA-LS SA-LS.hpp
-// g++ -std=c++20 -Wall --pedantic-errors -o main main.cpp
+// g++ -std=c++20 -Wall --pedantic-errors -O3 -o SA-LS SA-LS.hpp
+// g++ -std=c++20 -Wall --pedantic-errors -O3 -o SA-IS SA-IS.hpp
+// g++ -std=c++20 -Wall --pedantic-errors -O3 -o main main.cpp
 //-------------------------------------------------------------
 #include <iostream>
 #include <vector>
 #include <random>
 #include <chrono>
 #include "SA-LS.hpp"
+#include "SA-IS.hpp"
 
 void strGen(std::string & X, const int n, const std::vector<char> & alphabet, std::mt19937 & rng) { // ランダムに長さnの文字列を生成
     std::uniform_int_distribution<int> d(1, alphabet.size() - 1);
@@ -18,25 +20,36 @@ void strGen(std::string & X, const int n, const std::vector<char> & alphabet, st
 }
 
 int main() {
+    std::vector<char> alphabet = {'$', 'A', 'C', 'G', 'T'}; // 用途としてゲノム配列解析を想定している
+    std::string X; // Suffix Arrayを作る文字列
+    int n = 0; // 文字列の長さ
     std::mt19937 rng; // 乱数生成器
     std::random_device seed_gen;
     auto seed = seed_gen();
     rng.seed(seed);
 
-    std::vector<char> alphabet = {'$', 'A', 'C', 'G', 'T'}; // 用途としてゲノム配列解析を想定している
-    std::vector<int> testLen = {100, 1000, 10000, 100000, 1000000, 10000000};
-    for (auto L : testLen) {
-        std::string X;
-        strGen(X, L, alphabet, rng);
-        int n = X.length();
+    int mode = 1; // 0,1,2で指定
+    if (mode == 0) { // デバッグ用(自分で決めた文字列に対してsuffix arrayを作る)
+        X = "AAAAA$";
+        n = X.length();
         std::vector<int> V(n, 0); // inverse suffix array．groupを表す
         std::vector<int> I(n, 0); // 最終的なoutput．論文のrefinementに合わせてLの役割も兼ねている
-
-        auto start = clock();
         SALS(X, V, I, n, alphabet, rng);
-        auto end = clock();
-        auto CPU_time = double(end - start) / CLOCKS_PER_SEC;
-        std::cout << L << ": " << CPU_time << "\n";
+    }
+    else if (mode == 1) { // 時間計測用(ランダムに文字列を生成する)
+        std::vector<int> testLen = {100, 1000, 10000, 100000, 1000000, 10000000};
+        for (auto L : testLen) {
+            strGen(X, L, alphabet, rng);
+            n = X.length();
+            std::vector<int> V(n, 0); // inverse suffix array．groupを表す
+            std::vector<int> I(n, 0); // 最終的なoutput．論文のrefinementに合わせてLの役割も兼ねている
+
+            auto start = clock();
+            SALS(X, V, I, n, alphabet, rng);
+            auto end = clock();
+            auto CPU_time = double(end - start) / CLOCKS_PER_SEC;
+            std::cout << L << ": " << CPU_time << "\n";
+        }
     }
     return 0;
 }
