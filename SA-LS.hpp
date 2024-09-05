97d6d5069d0ea878c641a6458a3b71909ffbbad4
Tomoya Fujii <ftomoya2213@g.ecc.u-tokyo.ac.jp>
2024-09-05 17:04:34 +0900

    完成版

--- SA-LS.hpp
+++ SA-LS.hpp
@@ -1,13 +1,13 @@
 //-------------------------------------------------------------
 // Reference: N. Jesper Larsson and Kunihiko Sadakane. "Faster suffix sorting” Theoretical Computer Science, 387 (2007): 258-272.
-// To compile, perform: g++ -std=c++20 -Wall --pedantic-errors -o SA-LS SA-LS.hpp
+// To compile, perform: g++ -std=c++20 -Wall --pedantic-errors -O3 -o SA-LS SA-LS.hpp
 //-------------------------------------------------------------
 #pragma once
 #include <iostream>
 #include <vector>
 #include <random>
 
-void update_VandI(const std::string X, std::vector<int> & V, std::vector<int> & I, const int n, int & f, const int h, const int i, const int j) { // インデックスk=i~jの範囲でV[I[k]]とI[k]を更新する
+void update_VandI(const std::string & X, std::vector<int> & V, std::vector<int> & I, const int n, int & f, const int h, const int i, const int j) { // インデックスk=i~jの範囲でV[I[k]]とI[k]を更新する
     int key = j;
     for (int k = key; k >= i; k--) {
         if (h == 0 && X[abs(I[k]) + h] != X[abs(I[key]) + h]) key = k; // h=0のときは異なる文字が来たらkeyを更新
@@ -20,7 +20,7 @@ void update_VandI(const std::string X, std::vector<int> & V, std::vector<int> & 
     }
 }
 
-void init_VandI(const std::string X, std::vector<int> & V, std::vector<int> & I, const int n, const std::vector<char> alphabet, int & f, const int h) {
+void init_VandI(const std::string & X, std::vector<int> & V, std::vector<int> & I, const int n, const std::vector<char> alphabet, int & f, const int h) {
     int m = alphabet.size();
     std::vector<int> sum(m, 0); // alphabetの各文字の個数をカウント
     for (int i = 0; i < n; i++) {
@@ -43,7 +43,7 @@ void init_VandI(const std::string X, std::vector<int> & V, std::vector<int> & I,
 void tsQsort(std::vector<int> & V, std::vector<int> & I, const int h, const int i, const int j, std::mt19937 & rng) {
     if (i == j) return;
     std::uniform_int_distribution<> dist(i, j); // i以上j以下の整数を一様に生成する一様分布
-    int pivot = V[abs(I[dist(rng)]) + h]; // デバッグ用
+    int pivot = V[abs(I[dist(rng)]) + h];
     std::vector<int> S; // elements smaller than pivot
     std::vector<int> E; // elements equals to pivot
     std::vector<int> L; // elements larger than pivot
@@ -52,17 +52,17 @@ void tsQsort(std::vector<int> & V, std::vector<int> & I, const int h, const int 
         else if (V[abs(I[k]) + h] > pivot) L.push_back(I[k]);
         else E.push_back(I[k]);
     }
-    int l = S.size(); int r = L.size();
-    S.insert(S.end(), E.begin(), E.end()); // S,E,Lをこの順に連結
-    S.insert(S.end(), L.begin(), L.end());
+    int l = S.size(); int m = E.size(); int r = L.size();
     for (int k = i; k <= j; k++) { // I[i~j]を更新
-        I[k] = S[k - i];
+        if (k < i + l) I[k] = S[k - i]; // Sの要素をIに入れる
+        else if (i + l <= k && k < i + l + m) I[k] = E[k - i - l]; // Eの要素をIに入れる
+        else I[k] = L[k - i - l - m]; // Lの要素をIに入れる
     }
     if (l > 0) tsQsort(V, I, h, i, i + l - 1, rng); // Sだった要素を再帰的にソート
     if (r > 0) tsQsort(V, I, h, j - r + 1, j, rng); // Lだった要素を再帰的にソート
 }
 
-void SALS(const std::string X, std::vector<int> & V, std::vector<int> & I, const int n, const std::vector<char> alphabet, std::mt19937 & rng) {
+void SALS(const std::string & X, std::vector<int> & V, std::vector<int> & I, const int n, const std::vector<char> alphabet, std::mt19937 & rng) {
     int f = 0; // combined sorted groupの要素数．論文ではI[0]にその情報を格納していたが，ここでは変数1つを特別に使用する
     int h = 0; // h-order
     init_VandI(X, V, I, n, alphabet, f, h); // 論文のFig.1におけるstep(1)~(3)
